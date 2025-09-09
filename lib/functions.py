d84c313d7103484f95908130b4eaf266264ba67d
Kevin <kevinkoech007@gmail.com>
2025-09-09 22:38:54 +0300

    refactor greet_with_default function to handle None as a default name

--- lib/functions.py
+++ lib/functions.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python3
-
 def greet_programmer():
     print("Hello, programmer!")
 
 def greet(name):
     print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
-    print(f"Hello, {name}!")
+def greet_with_default(name=None):
+    if name is None:
+        print("Hello, programmer!")
+    else:
+        print(f"Hello, {name}!")
 
 def add(num1, num2):
     return num1 + num2
